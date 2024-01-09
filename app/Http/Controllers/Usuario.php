130e7e1a3b560c2158cf17a3977791c094cea01a
JuanMiguelElric <oliveirajuan243@gmail.com>
2024-01-09 10:42:14 -0300

    iniciando o meu crud

---
+++ app/Http/Controllers/Usuario.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class Usuario extends Controller
+{
+    public function Cadastro () {
+        //
+    }
+    public function Salvar (){
+        //git init
+    }
+    //
+}
